7ba7d95c8060513ee2e8e6f1da0bf2d61ab173de
Sumanpreet1313 <aujlasuman40@gmail.com>
2025-02-03 17:26:09 +0530

    Fixed Navbar import issue

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 import "./App.css";
 import {BrowserRouter as Router} from "react-router-dom";
 import { Toaster } from "react-hot-toast";
-import Navbar from "./components/Navbar";
+import Navbar from "./Components/Navbar.jsx";
 import HeroSection from "./components/HeroSection";
-import Services from "./components/Services";
-import About from "./components/About";
-import Contact from "./Components/Contact";
-import Footer from "./components/Footer";
+import Services from "./components/Services.jsx";
+import About from "./components/About.jsx";
+import Contact from "./components/Contact.jsx";
+import Footer from "./components/Footer.jsx";
 
 const App = () => {
   return (
